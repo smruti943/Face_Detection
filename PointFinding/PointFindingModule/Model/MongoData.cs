384aa32636e3f57f4219c2b770b29dde7fb99b50
Smruti Jena <01157095@pw.edu.pl>
2022-08-17 15:38:22 +0200

    Module 2

---
+++ PointFinding/PointFindingModule/Model/MongoData.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointFindingModule.Model
+{
+    public class MongoData
+    {
+        public ObjectId _id { get; set; }
+
+        public string fileName { get; set; }
+
+        public byte[] fileContent { get; set; }
+
+        
+    }
+}
